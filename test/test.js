7025adb83e09a08839d37333775dcfb4b85e08fd
xpiuma <36479435+xpiuma@users.noreply.github.com>
2019-05-27 17:45:11 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../index.js');
 describe('GET /', function() {
  it('respond with hello world', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('BIG hello world changed and deployed!!!', done);
+ request(app).get('/').expect('BIG hello world with PM2 restart', done);
  });
 });
