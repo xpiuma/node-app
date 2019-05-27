fd3ed21aea39ed222be741b6b140a0a9efcb7d0e
xpiuma <36479435+xpiuma@users.noreply.github.com>
2019-05-27 17:44:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('BIG hello world changed and deployed!!!');
+ res.send('BIG hello world with PM2 restart');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
